ce527204a1bcc424ebae8442498482b276da5900
Aloereed <58137529+Aloereed@users.noreply.github.com>
2023-02-23 11:48:36 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --opt-sub-quad-attention --lowvram --disable-nan-check
+set COMMANDLINE_ARGS= --opt-sub-quad-attention --disable-nan-check
 
 call webui.bat
